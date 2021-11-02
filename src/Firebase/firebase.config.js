5ddd3fa8dea2a842216e88a77080b6969c75c3a7
JannatRakhi <jannatrakhi31155@gmail.com>
2021-11-02 15:09:43 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDhyFmJlq2BMT_P7aJZWOR0tPvvBLvFIq8",
+  authDomain: "travelagency-project.firebaseapp.com",
+  projectId: "travelagency-project",
+  storageBucket: "travelagency-project.appspot.com",
+  messagingSenderId: "3163157416",
+  appId: "1:3163157416:web:5168fa4431c9428b02a39d"
+};
+export default firebaseConfig;
---
+++ src/components/Packages/Packages.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+
+const Packages = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('https://murmuring-fortress-49093.herokuapp.com/destinations')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+    return (
+        <div id="services" className='mb-5' >
+            <h1 className='text-dark text-center mx-5 my-5 '>Popular Destinations </h1>
+            <div className="row row-cols-1 row-cols-md-3 g-4 mx-5 m-auto">
+                {
+                    services.map(service => <Service
+                        key={service._id}
+                        service={service}></Service>)
+                }
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Packages;
